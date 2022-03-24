273637a5259e9be8f0e02b75e0928e43e6daeaef
Ian Gilham <igilham@users.noreply.github.com>
2022-03-24 16:58:07 +0000

    chore: whitespace clarity

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,8 @@ import { DateTime } from "luxon";
 
 export const getBaseURL = () =>
   "https://api.krakenflex.systems/interview-tests-mock-api/v1";
+
 export const getAPIKey = () => "EltgJ5G8m44IzwE6UN2Y4B4NjPW77Zk6FJK3lL23";
+
 export const getEarliestValidTimestamp = () =>
   DateTime.fromISO("2022-01-01T00:00:00.000Z");
